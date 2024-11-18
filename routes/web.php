db492a9b9005884357423c961ba423dfa89383a0
GeniBushati <56561750+GeniBushati@users.noreply.github.com>
2024-11-18 15:46:26 +0100

    First Commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\ReviewController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('books.index');
 });
+
+Route::resource('books', BookController::class);
+
+Route::resource('books.reviews', ReviewController::class)->scoped(['review'=>'book']);
